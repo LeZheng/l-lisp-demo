f95f425d54617426c344d68e55c45d99711c67e5
LeZheng <460163461@qq.com>
2017-10-29 21:56:38 +0800

    add additional-macro-and-fun.lisp file with ethe demo

---
+++ 31_Platform_Independent_Extensions/additional-macro-and-fun.lisp
@@ -0,0 +1,8 @@
+;;;This file show some additional fancy macros and functions examples
+
+(EXT:ETHE number (+ 1 2))
+;;=> 3 ;this will check the return value of form:(+ 1 2) 
+(EXT:ETHE number "This is string")
+;; ERROR ,becase form is a string
+
+
