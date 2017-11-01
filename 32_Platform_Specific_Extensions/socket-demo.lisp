f73e22a010dcafc6989de414f4057971c13e6f29
LeZheng <460163461@qq.com>
2017-11-02 07:00:59 +0800

    add wget-text,wget-binary and wput without test

--- 32_Platform_Specific_Extensions/socket-demo.lisp
+++ 32_Platform_Specific_Extensions/socket-demo.lisp
@@ -12,3 +12,41 @@
                                  (LOOP (WHEN (EQ :eof (SOCKET:SOCKET-STATUS (cons socket :input))) (RETURN))
                                        (format t "~A~%" (read-line socket)))))
          (SOCKET:SOCKET-SERVER-CLOSE server)))) 
+
+(DEFUN wget-text (host page file &OPTIONAL (port 80))
+       (WITH-OPEN-STREAM (socket (SOCKET:SOCKET-CONNECT port host :EXTERNAL-FORMAT :DOS))
+                         (FORMAT socket "GET ~A HTTP/1.0~2%" page)
+                         (WITH-OPEN-FILE (out file :direction :output)
+                                         (LOOP :for line = (READ-LINE socket nil nil) :while line
+                                               :do (WRITE-LINE line out)))))
+
+(DEFUN wget-binary (host page file &OPTIONAL (port 80))
+       (WITH-OPEN-STREAM 
+         (socket (SOCKET:SOCKET-CONNECT port host :EXTERNAL-FORMAT :DOS))
+         (FORMAT socket "GET ~A HTTP/1.0~2%" page)
+         (LOOP :with content-length :for line = (READ-LINE socket nil nil)
+               :until (ZEROP (LENGTH line)) :do
+               (WHEN (STRING= line #1="Content-length: " :end1 #2=#.(LENGTH #1#))
+                     (SETQ content-length (PARSE-INTEGER line :start #2#))
+                     :finally (RETURN (LET ((data (MAKE-ARRAY content-length
+                                                              :element-type '(UNSIGNED-BYTE 8))))
+                                           (SETF (STREAM-ELEMENT-TYPE socket) '(UNSIGNED-BYTE 8))
+                                           (EXT:READ-BYTE-SEQUENCE data socket)
+                                           (WITH-OPEN-FILE (out file :direction :output
+                                                                :ELEMENT-TYPE '(UNSIGNED-BYTE 8))
+                                                           (EXT:WRITE-BYTE-SEQUENCE data out))
+                                           data))))))
+
+(DEFUN wput (host page file &OPTIONAL (port 80))
+       (WITH-OPEN-STREAM 
+         (socket (SOCKET:SOCKET-CONNECT port host :EXTERNAL-FORMAT :DOS))
+         (WITH-OPEN-FILE 
+           (in file :direction :inptut :ELEMENT-TYPE '(UNSIGNED-BYTE 8))
+           (LET* ((length (FILE-LENGTH in))
+                  (data (MAKE-ARRAY length :element-type '(UNSIGNED-BYTE 8))))
+                 (FORMAT socket "PUT ~A HTTP/1.0~%Content-length: ~D~2%" page length)
+                 (SETF (STREAM-ELEMENT-TYPE socket) '(UNSIGNED-BYTE 8))
+                 (EXT:READ-BYTE-SEQUENCE data in)
+                 (EXT:WRITE-BYTE-SEQUENCE data socket)))
+         (SOCKET:SOCKET-STREAM-SHUTDOWN socket :output)
+         (LOOP :for line = (READ-LINE socket nil nil) :while line :collect line)))
