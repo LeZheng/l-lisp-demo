1f0e70e4d534367ddf438f14dc06d481005d7886
LeZheng <460163461@qq.com>
2019-10-27 16:06:58 +0800

    添加测试代码

--- Data_Structure_and_Algorithms/sort.lisp
+++ Data_Structure_and_Algorithms/sort.lisp
@@ -2,41 +2,41 @@
 (defun bubble-sort (arr)
    (dotimes (i (1- (length arr)) arr)
      (dotimes (j (- (length arr) i 1))
-       (if (> (elt arr j) (elt arr (1+ j)))
-         (rotatef (elt arr j) (elt arr (1+ j)))))))
+       (if (> (svref arr j) (svref arr (1+ j)))
+         (rotatef (svref arr j) (svref arr (1+ j)))))))
 
 (defun bubble-sort-2 (arr &optional (start 0))
   (if (>= start (1- (length arr)))
     arr
     (progn (loop for i from (1- (length arr)) above start
-                      do (if (< (elt arr i) (elt arr (1- i))) (rotatef (elt arr i) (elt arr (1- i)))))
+                      do (if (< (svref arr i) (svref arr (1- i))) (rotatef (svref arr i) (svref arr (1- i)))))
            (bubble-sort-2 arr (1+ start)))))
 
 ;;2,插入排序
 (defun insert-sort (arr)
   (dotimes (i (1- (length arr)) arr)
-    (loop for j from (1+ i) downto 1 do (if (< (elt arr j) (elt arr (1- j)))
-                                          (rotatef (elt arr j) (elt arr (1- j)))
+    (loop for j from (1+ i) downto 1 do (if (< (svref arr j) (svref arr (1- j)))
+                                          (rotatef (svref arr j) (svref arr (1- j)))
                                           (return)))))
 
 (defun insert-sort-2 (arr &optional (start 0))
   (if (= start (length arr))
     arr
-    (loop for i from start downto 1 until (> (elt arr i) (elt arr (1- i))) do (rotatef (elt arr i) (elt arr (1- i)))
+    (loop for i from start downto 1 until (> (svref arr i) (svref arr (1- i))) do (rotatef (svref arr i) (svref arr (1- i)))
           finally (return (insert-sort-2 arr (1+ start))))))
 
 ;;3,选择排序
 (defun select-sort (arr)
   (dotimes (i (1- (length arr)) arr)
     (let ((min-index i))
-      (loop for j from i to (1- (length arr)) do (if (< (elt arr j) (elt arr min-index)) (setf min-index j)))
-      (rotatef (elt arr i) (elt arr min-index)))))
+      (loop for j from i to (1- (length arr)) do (if (< (svref arr j) (svref arr min-index)) (setf min-index j)))
+      (rotatef (svref arr i) (svref arr min-index)))))
 
 (defun select-sort-2 (arr &optional (start 0))
   (if (>= start (1- (length arr)))
     arr
-    (loop with min-index = start for i from start to (1- (length arr)) do (if (< (elt arr i) (elt arr min-index)) (setf min-index i))
-          finally (progn (rotatef (elt arr min-index) (elt arr start))
+    (loop with min-index = start for i from start to (1- (length arr)) do (if (< (svref arr i) (svref arr min-index)) (setf min-index i))
+          finally (progn (rotatef (svref arr min-index) (svref arr start))
                          (return (select-sort-2 arr (1+ start)))))))
 
 ;;4,希尔排序
@@ -45,8 +45,8 @@
     ((< gap 1) arr)
     (dotimes (i gap)
       (dotimes (k (1- (floor (length arr) gap)))
-        (loop for j from (+ i (* gap k)) downto 0 by gap do (if (< (elt arr (+ j gap)) (elt arr j))
-                                                                 (rotatef (elt arr j) (elt arr (+ j gap)))
+        (loop for j from (+ i (* gap k)) downto 0 by gap do (if (< (svref arr (+ j gap)) (svref arr j))
+                                                                 (rotatef (svref arr j) (svref arr (+ j gap)))
                                                                  (return)))))))
 
 ;;5,归并排序
@@ -92,13 +92,25 @@
                     (let ((left (+ (* 2 current-index) 1))
                           (right (+ (* 2 current-index) 2))
                           (max current-index))
-                      (if (and (< left size) (> (elt arr left) (elt arr max))) (setf max left))
-                      (if (and (< right size) (> (elt arr right) (elt arr max))) (setf max right))
+                      (if (and (< left size) (> (svref arr left) (svref arr max))) (setf max left))
+                      (if (and (< right size) (> (svref arr right) (svref arr max))) (setf max right))
                       (when (/= current-index max)
-                        (rotatef (elt arr max) (elt arr current-index))
+                        (rotatef (svref arr max) (svref arr current-index))
                         (heapify arr max size)))))
     (dotimes (i (length arr) arr)
       (let ((max-size (- (length arr) i)))
         (loop for j from (1- max-size) downto 0
               do (heapify arr j max-size))
-        (rotatef (elt arr 0) (elt arr (1- max-size)))))))
+        (rotatef (svref arr 0) (svref arr (1- max-size)))))))
+
+
+(defvar *test-seq* #(1 4 0 2 3 8 5 3 33 77 88 9 11))
+
+(defun test-correctness ()
+  (let ((funs (list 'bubble-sort 'bubble-sort-2 'insert-sort 'insert-sort-2 
+                    'select-sort 'select-sort-2 'quick-sort 'heap-sort 
+                    'radix-sort 'shell-sort 'merge-sort)))
+    (dolist (fun funs)
+      (if (equalp (sort (copy-seq *test-seq*) #'<) (funcall (symbol-function fun) (copy-seq *test-seq*)))
+        (format t "~A test ok~%" (symbol-name fun))
+        (format t "~A test failed~%" (symbol-name fun))))))
