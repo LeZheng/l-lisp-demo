e98bc4ce7840a9892e89753e8de7203ae8c38c64
LeZheng <460163461@qq.com>
2019-11-07 10:06:42 +0800

    修复kmp的临界状态问题

--- Data_Structure_and_Algorithms/kmp.lisp
+++ Data_Structure_and_Algorithms/kmp.lisp
@@ -1,16 +1,17 @@
 (defun kmp-match (str p &optional (start 0) (next nil))
-  (if (or (= (length p) 0) (> start (- (length str) (length p))))
-    nil
-    (let ((next-array (or next (loop for i from 2 below (length p) ;TODO 这里的2存在问题
-                                     with arr = (make-array (length p) :fill-pointer 2 :initial-element 0)
-                                     do (vector-push (if (equal (char p (1- i)) (char p (aref arr (1- i)))) 
-                                                       (1+ (aref arr (1- i))) 
-                                                       0)
-                                                arr)
-                          finally (return arr)))))
-      (loop for i from start below (length str)
-            and c across p
-            until (not (equal c (char str i)))
-            finally (if (>= (- i start)  (length p))
-                      (return start)
-                      (return (kmp-match str p (+ start (1+ (aref next-array (- i start)))) next-array)))))))
+  (cond 
+    ((or (= (length p) 0) (> start (- (length str) (length p)))) nil)
+    ((= (length p) 1) (position (char p 0) str))
+    (t (let ((next-array (or next (loop for i from 2 below (length p)
+                                        with arr = (make-array (length p) :fill-pointer 2 :initial-element 0)
+                                        do (vector-push (if (equal (char p (1- i)) (char p (aref arr (1- i)))) 
+                                                          (1+ (aref arr (1- i))) 
+                                                          0)
+                                                        arr)
+                                        finally (return arr)))))
+         (loop for i from start below (length str)
+               and c across p
+               until (not (equal c (char str i)))
+               finally (if (>= (- i start)  (length p))
+                         (return start)
+                         (return (kmp-match str p (+ start (1+ (aref next-array (- i start)))) next-array))))))))
