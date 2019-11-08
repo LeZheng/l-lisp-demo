86c95e05c456f570a6b011ca4338db716c2dc621
LeZheng <460163461@qq.com>
2019-11-08 16:51:11 +0800

    重写KMP算法

--- Data_Structure_and_Algorithms/kmp.lisp
+++ Data_Structure_and_Algorithms/kmp.lisp
@@ -1,18 +1,15 @@
-(defun kmp-match (str p)
-  (if (> (length p) 0)
-    (let ((next-array (loop for i from 2 below (length p)
-                            with arr = (make-array (1+ (length p)) :fill-pointer 2 :initial-element 0)
-                            do (vector-push (if (equal (char p (1- i)) (char p (aref arr (1- i)))) 
-                                              (1+ (aref arr (1- i))) 
-                                              0)
-                                            arr)
-                            finally (return arr))))
-      (labels ((match (start)
-                      (loop for i from start below (length str)
-                            and c across p
-                            until (not (equal c (char str i)))
-                            finally (return (cond 
-                                              ((>= (- i start)  (length p)) start)
-                                              ((= start (- (length str) (length p))) nil)
-                                              (t (match (+ start 1 (aref next-array (- i start))))))))))
-        (match 0)))))
+(defun index-kmp (str p)
+  (let ((next (loop for i from 1 below (length p)
+                    with arr = (make-array (length p) :initial-element 0)
+                    with j = 0
+                    do (cond 
+                         ((= i 1) (setf (aref arr i) 1))
+                         ((equal (elt p (1- i)) (elt p j)) (psetf j (1+ j) (aref arr i) (1+ (aref arr (1- i)))))
+                         (t (setf j 0 (aref arr i) 1)))
+                    finally (return arr))))
+    (loop with i = 0 and j = 0
+          while (and (< i (length str)) (< j (length next)))
+          when (equal (elt str i) (elt p j)) do (if (= j (1- (length next))) 
+                                                    (return (- i j)) 
+                                                    (progn (incf i) (incf j)))
+          else do (setf i (1+ i) j (max 0 (1- (aref next j)))))))
