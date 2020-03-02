eef1fb65d03223303980e8b94c88390764df75d6
LeZheng <460163461@qq.com>
2020-03-02 21:37:32 +0800

    complet until 2.62

--- SICP_Exercise/chapter-two.lisp
+++ SICP_Exercise/chapter-two.lisp
@@ -1144,3 +1144,34 @@
 
 (defun union-set (set1 set2)
   (append set1 set2))
+
+;;;有序集合
+(defun element-of-set? (x set)
+  (cond ((null set) nil)
+        ((= x (car set)) t)
+        ((< x (car set)) nil)
+        (t (element-of-set? x (cdr set)))))
+
+(defun intersection-set (set1 set2)
+  (if (or (null set1) (null set2))
+    '()
+    (let ((x1 (car set1)) (x2 (car set2)))
+      (cond ((= x1 x2) (cons x1 (intersection-set (cdr set1) (cdr set2))))
+            ((< x1 x2) (intersection-set (cdr set1) set2))
+            ((> x1 x2) (intersection-set set1 (cdr set2)))))))
+
+;;;exercice 2.61
+(defun adjoin-set (x set)
+  (cond ((null set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        ((> x (car set)) (cons (car set) (adjoin-set x (cdr set))))))
+
+;;;exercice 2.62
+(defun union-set (set1 set2)
+  (cond ((null set1) set2)
+        ((null set2) set1)
+        (t (let ((x1 (car set1)) (x2 (car set2)))
+             (cond ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
+                   ((< x1 x2) (cons x1 (union-set (cdr set1) set2)))
+                   ((> x1 x2) (cons x2 (union-set set1 (cdr set2)))))))))
