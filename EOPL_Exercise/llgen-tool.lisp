76ea28e2524f562e4a41c8c461d2d7dcf27a78b3
LeZheng <460163461@qq.com>
2023-02-01 07:18:32 +0800

    update llgen-tool

--- EOPL_Exercise/llgen-tool.lisp
+++ EOPL_Exercise/llgen-tool.lisp
@@ -12,170 +12,103 @@
     (identifier (letter (arbno (or letter digit))) symbol)
     (number (digit (arbno digit)) number)))
 
-(defun re-read (v)
-  (if (vectorp v)
-      (let ((buffer (svref v 1))
-	    (fun (svref v 0)))
-	(if buffer
-	    (cond
-	      ((functionp fun) (re-read (vector (funcall fun (car buffer)) (cdr buffer))))
-	      ((vectorp fun) (vector (svref fun 0) (append (svref fun 1) buffer)))
-	      (t v))
-	    fun))
-      v))
+(defstruct scan-result token-chars remain-chars)
 
 (defun make-string-scanner (scanner-spec)
   (labels
-      ((make-scanner (impl token-reducer)
-	 (lambda (c) (if c
-			 (funcall impl c)
-			 (vector (funcall token-reducer nil) nil))))
-       (regexp->scanner (regexp token-reducer chars-reducer)
+      ((concat-reducer (reducer c)
+	 (lambda (s) (funcall reducer (cons c s))))
+       (regexp->scanner (regexp reducer next-cont)
 	 (if (null regexp)
-	     (vector (funcall token-reducer nil) nil)
-	     (let ((exp (car regexp)))
-	       (cond
-		 ((consp exp)
-
-		  (regexp->scanner exp
-				   (lambda (s) (regexp->scanner
-						(cdr regexp)
-						(lambda (s2) (funcall token-reducer (append s s2)))
-						chars-reducer))
-				   chars-reducer))
-		 ((stringp exp)
-		  (regexp->scanner (cons (cons 'concat (coerce exp 'list)) (cdr regexp)) token-reducer chars-reducer))
-		 ((characterp exp)
-		  (make-scanner (lambda (c)
-				  (if (equal exp c)
-				      (regexp->scanner (cdr regexp)
-						       (lambda (s) (funcall token-reducer (cons c s)))
-						       (lambda (s) (funcall chars-reducer (cons c s))))
-				      (vector nil (funcall chars-reducer (cons c nil)))))
-				token-reducer))
-		 ((eql exp 'letter)
-		  (make-scanner (lambda (c)
-				  (if (alpha-char-p c)
-				      (regexp->scanner (cdr regexp)
-						       (lambda (s) (funcall token-reducer (cons c s)))
-						       (lambda (s) (funcall chars-reducer (cons c s))))
-				      (vector nil (funcall chars-reducer (cons c nil)))))
-				token-reducer))
-		 ((eql exp 'digit)
-		  (make-scanner (lambda (c)
-				  (if (digit-char-p c)
-				      (regexp->scanner (cdr regexp)
-						       (lambda (s) (funcall token-reducer (cons c s)))
-						       (lambda (s) (funcall chars-reducer (cons c s))))
-				      (vector nil (funcall chars-reducer (cons c nil)))))
-				token-reducer))
-		 ((eql exp 'any)
-		  (make-scanner (lambda (c)
-				  (regexp->scanner (cdr regexp)
-						   (lambda (s) (funcall token-reducer (cons c s)))
-						   (lambda (s) (funcall chars-reducer (cons c s)))))
-				token-reducer))
-		 ((eql exp 'whitespace)
-		  (regexp->scanner (cons '(concat (or #\Space #\NewLine) (arbno (or #\Space #\NewLine))) (cdr regexp))
-				   token-reducer chars-reducer))
-		 ((eql exp 'not)
-		  (make-scanner (lambda (c)
-				  (if (not (equal c (cadr regexp)))
-				      (funcall token-reducer (cons c nil))
-				      (vector nil (funcall chars-reducer (cons c nil)))))
-				token-reducer))
-		 ((eql exp 'or)
-		  (labels ((make-or-scanner (scanners)
-			     (make-scanner (lambda (c)
-					     (let ((results (mapcar (lambda (scanner)
-								      (if (functionp scanner)
-									  (funcall scanner c)
-									  scanner))
-								    scanners)))
-					       (let ((result (find-if (lambda (s) (and (vectorp s) (consp (svref s 0)))) results)))
-						 ;;(format t "or scan :~A -> ~A~%" c results)
-						 (if result
-						     (re-read (vector (funcall token-reducer (svref result 0)) (svref result 1)))
-						     (if (some #'functionp results)
-							 (make-or-scanner results)
-							 (vector nil (funcall chars-reducer (cons c nil))))))))
-					   token-reducer)))
-		    (make-or-scanner (mapcar (lambda (exp)
-					       (regexp->scanner (cons exp nil) #'identity #'identity))
-					     (cdr regexp)))))
-		 ((eql exp 'arbno)
-		  (make-scanner (lambda (c)
-				  (let ((r (funcall (regexp->scanner (cdr regexp) #'identity #'identity) c)))
-				    (labels ((handle-result (r reducer)
-					       ;;(format t "handle result: ~A~%" r)
-					       (etypecase r
-						 (cons (regexp->scanner regexp
-									(lambda (s) (funcall token-reducer (append r s)))
-									(lambda (s) (funcall chars-reducer (append (funcall reducer nil) s)))))
-						 (function
-						  (make-scanner (lambda (c)
-								  (handle-result (funcall r c) (lambda (s) (funcall reducer (cons c s)))))
-								#'identity))
-						 (vector
-						  (if (null (svref r 0))
-						      (re-read (vector (funcall token-reducer nil) (svref r 1)))
-						      (re-read (vector
-								(regexp->scanner regexp
-										 (lambda (s) (funcall token-reducer (append (svref r 0) s)))
-										 (lambda (s) (funcall chars-reducer (append (funcall reducer nil) s))))
-								(svref r 1))))))))
-				      (handle-result r #'identity))))
-				token-reducer))
-		 ((eql exp 'concat) (regexp->scanner (cdr regexp) token-reducer chars-reducer))
-		 (t (error "Unknown expression:~A~%" exp)))))))
-    (let ((scanner-list (mapcar (lambda (spec)
-				  (destructuring-bind (name regexp action) spec
-				    (regexp->scanner regexp
-						     (lambda (s)
-						       (let ((token-str (coerce s 'string)))
-							 (ecase action
-							   (skip (cons name nil))
-							   (number (cons name (read-from-string token-str)))
-							   (symbol (cons name (make-symbol token-str)))
-							   (string (cons name token-str)))))
-						     #'identity)))
-				scanner-spec))
-	  (token-list '()))
-      (lambda (text)
-	(labels ((iter-char (chars scanners tokens)
-		   (format t "iter-char:~A -> ~A ~% tokens: ~A~%" (car chars) scanners tokens)
-		   (if (some #'functionp scanners)
-		       (iter-char (cdr chars)
-				  (mapcar
-				   (lambda (scanner)
-				     (etypecase scanner
-				       (function (let ((next (funcall scanner (car chars))))
-						   (format t "next: ~A~%" next)
-						   (etypecase next
-						     (cons (vector next (cdr chars)))
-						     (function next)
-						     (vector (re-read (vector (svref next 0) (append (svref next 1) (cdr chars))))))))
-				       (t scanner)))
-				   scanners)
-				  tokens)
-		       (let ((token-result (reduce (lambda (&optional s1 s2)
-						     ;;(format t "reduce: ~A ~A~%" s1 s2)
-						     (if (> (length (svref s1 1)) (length (svref s2 1)))
-							 s2
-							 s1))
-						   scanners)))
-			 (if (null (svref token-result 0))
-			     (error "Can not handle: ~A~%" (svref token-result 1))
-			     (if (and (null chars) (null (svref token-result 1)))
-				 (reverse tokens)
-				 (iter-char (svref token-result 1)
-					    scanner-list
-					    (if (null (svref token-result 0))
-						tokens
-						(cons (svref token-result 0) tokens)))))))))
-	  (iter-char (coerce text 'list) scanner-list '()))))))
-
-
+	     (funcall next-cont (funcall reducer nil) nil)
+	     (lambda (c)
+	       (labels
+		   ((handle-single-char-with (predicate)
+		      (if (funcall predicate c)
+			  (regexp->scanner (cdr regexp) (concat-reducer reducer c) next-cont)
+			  (funcall next-cont nil (funcall remain-reducer `(,c))))))
+		 (let ((exp (car regexp)))
+		   (etypecase exp
+		     (cons
+		      (regexp->scanner exp #'identity
+				       (lambda (token-chars remain-chars)
+					 (if token-chars
+					     (regexp->scanner (cdr regexp)
+							      (lambda (s) (funcall reducer (append token-chars s)))
+							      next-cont)
+					     (funcall next-cont nil (funcall reducer remain-chars))))))
+		     (string
+		      (regexp->scanner (cons (cons 'concat (coerce exp 'list)) (cdr regexp)) reducer next-cont))
+		     (character
+		      (handle-single-char-with (lambda (c) (equal exp c))))
+		     (symbol
+		      (ecase exp
+			(letter
+			 (handle-single-char-with #'alpha-char-p))
+			(digit
+			 (handle-single-char-with #'digit-char-p))
+			(whitespace
+			 (regexp->scanner (cons '(concat (or #\Space #\NewLine) (arbno (or #\Space #\NewLine))) (cdr regexp))
+					  reducer next-cont))
+			(any
+			 (handle-single-char-with (lambda (c) t)))
+			(not
+			 (if (not (equal c (cadr regexp)))
+			     (funcall next-cont (funcall remain-chars `(,c)) nil)
+			     (funcall next-cont nil (funcall remain-chars `(,c)))))
+			(or
+			 (labels ((apply-scanners (scanners c)
+				    (let ((scanners (mapcar
+						     (lambda (scanner)
+						       (if (functionp scanner)
+							   (funcall scanner c)
+							   scanner))
+						     scanners)))
+				      (if (some #'functionp scanners)
+					  (lambda (c) (apply-scanners c scanners))
+					  (let ((result (reduce
+							 (lambda (&optional a b)
+							   (if (and a b)
+							       (if (> (length (car a)) (length (car b))) a b)))
+							 scanners)))
+					    (funcall next-cont (car result) (cdr result)))))))
+			   (apply-scanners c
+					   (mapcar
+					    (if (eql scanner-spec (cdr regexp))
+						(lambda (spec)
+						  (regexp->scanner
+						   (cadr spec)
+						   #'identity
+						   (lambda (token-chars remain-chars)
+						     (cons (list (car spec) token-chars (caddr spec)) remain-chars))))
+						(lambda (sub-exp) (regexp->scanner sub-exp #'identity #'cons)))
+					    (cdr regexp)))))
+			(arbno
+			 (regexp->scanner
+			  (cdr regexp) #'identity
+			  (lambda (token-chars remain-chars)
+			    (if token-chars
+				(regexp->scanner
+				 regexp
+				 #'identity
+				 (lambda (tc2 rc2)
+				   (if tc2
+				       (funcall next-cont (funcall reducer (append token-chars tc2)) rc2)
+				       (funcall next-cont (funcall reducer token-chars) rc2))))
+				(funcall next-cont nil (funcall reducer rc2))))))
+			(concat
+			 (regexp->scanner (cdr regexp) reducer next-cont)))))))))))
+    (lambda (text)
+      (let ((src-scanner (regexp->scanner (cons 'or scanner-spec) #'identity #'cons)))
+	(labels
+	    ((iter-char (chars scanner tokens)
+	       (if chars
+		   (let ((r (funcall scanner (car chars))))
+		     (if (functionp r)
+			 (iter-char (cdr chars) r tokens)
+			 (iter-char (append (cdr r) (cdr chars)) src-scanner (cons (car r) tokens))))
+		   (reverse tokens))))
+	  (iter-char (coerce text 'list) src-scanner nil))))))
 
 (setf the-lexical-spec
       '((whitespace ((or #\Space #\NewLine) (arbno (or #\Space #\NewLine))) skip)
